983983538a6bd0d74b021bad5321c88db6c870f8
Frank Xu <frank030122@gmail.com>
2021-10-24 11:39:10 -0500

    Update setting.py

--- Project/MAP/setting.py
+++ Project/MAP/setting.py
@@ -7,8 +7,8 @@ GREEN = (0, 255, 0)
 YELLOW = (255, 255, 0)
 
 #screen, window, and game settings
-WIDTH = 1024
-HEIGHT = 768
+WIDTH = 1024   #32 * 32
+HEIGHT = 768   #32 * 24
 FPS = 120
 TITLE = "Map_test1"
 BGCOLOR = DARKGREY #backgroud color
